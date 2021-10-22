3dc224e19e0b5a86682bc1fde84ac176bea38312
chuyanwu <92982961+chuyanwu@users.noreply.github.com>
2021-10-22 22:35:57 +0800

    R script

--- Figure 6A .ggalluvial.R
+++ Figure 6A .ggalluvial.R
@@ -1,31 +1,27 @@
-######Video source: https://ke.biowolf.cn
-######ÉúĞÅ×ÔÑ§Íø: https://www.biowolf.cn/
-######Î¢ĞÅ¹«ÖÚºÅ£ºbiowolf_cn
-######ºÏ×÷ÓÊÏä£ºbiowolf@foxmail.com
-######´ğÒÉÎ¢ĞÅ: 18520221056
+
 
 #install.packages("ggplot2")
 #install.packages("ggalluvial")
 
 
-#ÒıÓÃ°ü
+#å¼•ç”¨åŒ…
 library(ggalluvial)
 library(ggplot2)
 library(dplyr)
-m6aCluFile="m6aCluster.txt"         #m6A·ÖĞÍÎÄ¼ş
-geneCluFile="geneCluster.txt"       #»ùÒò·ÖĞÍÎÄ¼ş
-scoreFile="m6Ascore.group.txt"      #m6A´ò·ÖµÄ·Ö×éÎÄ¼ş
-cliFile="clinical.txt"              #ÁÙ´²Êı¾İÎÄ¼ş
-trait="Fustat"                      #ÁÙ´²ĞÔ×´
-setwd("D:\\biowolf\\m6aTME\\39.ggalluvial")     #ÉèÖÃ¹¤×÷Ä¿Â¼
+m6aCluFile="m6aCluster.txt"         #m6Aåˆ†å‹æ–‡ä»¶
+geneCluFile="geneCluster.txt"       #åŸºå› åˆ†å‹æ–‡ä»¶
+scoreFile="m6Ascore.group.txt"      #m6Aæ‰“åˆ†çš„åˆ†ç»„æ–‡ä»¶
+cliFile="clinical.txt"              #ä¸´åºŠæ•°æ®æ–‡ä»¶
+trait="Fustat"                      #ä¸´åºŠæ€§çŠ¶
+setwd("D:\\biowolf\\m6aTME\\39.ggalluvial")     #è®¾ç½®å·¥ä½œç›®å½•
 
-#¶ÁÈ¡ÊäÈëÎÄ¼ş
+#è¯»å–è¾“å…¥æ–‡ä»¶
 m6aClu=read.table(m6aCluFile, header=T, sep="\t", check.names=F, row.names=1)
 geneClu=read.table(geneCluFile, header=T, sep="\t", check.names=F, row.names=1)
 score=read.table(scoreFile, header=T, sep="\t", check.names=F, row.names=1)
 cli=read.table(cliFile, header=T, sep="\t", check.names=F, row.names=1)
 
-#ºÏ²¢Êı¾İ
+#åˆå¹¶æ•°æ®
 twoCluster=cbind(m6aClu, geneClu)
 rownames(twoCluster)=gsub("(.*?)\\_(.*?)", "\\2", rownames(twoCluster))
 sameSample=intersect(row.names(twoCluster), row.names(score))
@@ -33,32 +29,26 @@ scoreClu=cbind(score[sameSample,,drop=F], twoCluster[sameSample,,drop=F])
 sameSample=intersect(row.names(scoreClu), row.names(cli))
 rt=cbind(scoreClu[sameSample,], cli[sameSample,])
 
-#×¼±¸É£»ùÍ¼ÊäÈëÎÄ¼ş
+#å‡†å¤‡æ¡‘åŸºå›¾è¾“å…¥æ–‡ä»¶
 rt=rt[,c("m6Acluster", "geneCluster", "group", trait)]
 colnames(rt)=c("m6Acluster", "geneCluster", "m6Ascore", trait)
 corLodes=to_lodes_form(rt, axes = 1:ncol(rt), id = "Cohort")
 
-#µÃµ½Êä³öÎÄ¼ş
+#å¾—åˆ°è¾“å‡ºæ–‡ä»¶
 pdf(file="ggalluvial.pdf", width=6, height=6)
 mycol=rep(c("#0066FF","#FF9900","#FF0000","#029149","#6E568C","#E0367A","#D8D155","#223D6C","#D20A13","#431A3D","#91612D","#FFD121","#088247","#11AA4D","#58CDD9","#7A142C","#5D90BA","#64495D","#7CC767"),15)
 ggplot(corLodes, aes(x = x, stratum = stratum, alluvium = Cohort,fill = stratum, label = stratum)) +
   	 scale_x_discrete(expand = c(0, 0)) +  
-  	 #ÓÃaes.flow¿ØÖÆÏßÌõÑÕÉ«£¬forwardËµÃ÷ÑÕÉ«ºÍÇ°ÃæµÄÖù×´Í¼Ò»ÖÂ£¬backwardËµÃ÷ºÍºóÃæµÄÖù×´Í¼Ò»ÖÂ¡£
+  	 #ç”¨aes.flowæ§åˆ¶çº¿æ¡é¢œè‰²ï¼Œforwardè¯´æ˜é¢œè‰²å’Œå‰é¢çš„æŸ±çŠ¶å›¾ä¸€è‡´ï¼Œbackwardè¯´æ˜å’Œåé¢çš„æŸ±çŠ¶å›¾ä¸€è‡´ã€‚
   	 geom_flow(width = 2/10,aes.flow = "forward") + 
 	 geom_stratum(alpha = .9,width = 2/10) +
 	 scale_fill_manual(values = mycol) +
-	 #size=3´ú±í×ÖÌå´óĞ¡
+	 #size=3ä»£è¡¨å­—ä½“å¤§å°
 	 geom_text(stat = "stratum", size = 3,color="black") +
 	 xlab("") + ylab("") + theme_bw() + 
-	 theme(axis.line = element_blank(),axis.ticks = element_blank(),axis.text.y = element_blank()) + #È¥µô×ø±êÖá
+	 theme(axis.line = element_blank(),axis.ticks = element_blank(),axis.text.y = element_blank()) + #å»æ‰åæ ‡è½´
 	 theme(panel.grid =element_blank()) + 
 	 theme(panel.border = element_blank()) + 
 	 ggtitle("") + guides(fill = FALSE)                            
 dev.off()
 
-
-######Video source: https://ke.biowolf.cn
-######ÉúĞÅ×ÔÑ§Íø: https://www.biowolf.cn/
-######Î¢ĞÅ¹«ÖÚºÅ£ºbiowolf_cn
-######ºÏ×÷ÓÊÏä£ºbiowolf@foxmail.com
-######´ğÒÉÎ¢ĞÅ: 18520221056
