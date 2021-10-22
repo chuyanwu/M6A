9d990641dd2b837961ef56a383f15d5cec44c148
chuyanwu <92982961+chuyanwu@users.noreply.github.com>
2021-10-22 20:05:55 +0800

    R script

---
+++ Figure 1D.m6Adiff.R
@@ -0,0 +1,76 @@
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("limma")
+
+#install.packages("reshape2")
+#install.packages("ggpubr")
+
+
+#引用包
+library(limma)
+library(reshape2)
+library(ggpubr)
+expFile="TCGA.TPM.txt"      #基因表达矩阵
+geneFile="gene.txt"         #m6A基因列表
+setwd("D:\\biowolf\\m6aTME\\15.m6Adiff")       #设置工作目录
+
+#读取输入文件
+rt=read.table(expFile, header=T, sep="\t", check.names=F)
+rt=as.matrix(rt)
+rownames(rt)=rt[,1]
+exp=rt[,2:ncol(rt)]
+dimnames=list(rownames(exp), colnames(exp))
+data=matrix(as.numeric(as.matrix(exp)), nrow=nrow(exp), dimnames=dimnames)
+data=avereps(data)
+
+#提取m6A基因表达量
+gene=read.table(geneFile, header=T, sep="\t", check.names=F)
+sameGene=intersect(as.vector(gene[,1]), row.names(data))
+data=data[sameGene,]
+
+#正常和肿瘤数目
+group=sapply(strsplit(colnames(data),"\\-"), "[", 4)
+group=sapply(strsplit(group,""), "[", 1)
+group=gsub("2", "1", group)
+conNum=length(group[group==1])       #正常组样品数目
+treatNum=length(group[group==0])     #肿瘤组样品数目
+sampleType=c(rep(1,conNum), rep(2,treatNum))
+
+#把数据转换成ggplot2输入文件
+exp=log2(data+1)
+exp=as.data.frame(t(exp))
+exp=cbind(exp, Type=sampleType)
+exp$Type=ifelse(exp$Type==1, "Normal", "Tumor")
+data=melt(exp, id.vars=c("Type"))
+colnames(data)=c("Type", "Gene", "Expression")
+
+#绘制箱线图
+p=ggboxplot(data, x="Gene", y="Expression", color = "Type", 
+	     ylab="Gene expression",
+	     xlab="",
+	     legend.title="Type",
+	     palette = c("blue", "red"),
+	     width=1)
+p=p+rotate_x_text(60)
+p1=p+stat_compare_means(aes(group=Type),
+	      method="wilcox.test",
+	      symnum.args=list(cutpoints = c(0, 0.001, 0.01, 0.05, 1), symbols = c("***", "**", "*", " ")),
+	      label = "p.signif")
+
+#输出箱线图
+pdf(file="boxplot.pdf", width=7, height=5)
+print(p1)
+dev.off()
+
+
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
