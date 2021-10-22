d20196f83418f17aa38edb583e6e31be58f2a932
chuyanwu <92982961+chuyanwu@users.noreply.github.com>
2021-10-22 22:53:50 +0800

    R script

--- Figure 7E and 7F .maftools.R
+++ Figure 7E and 7F .maftools.R
@@ -1,49 +1,37 @@
-######Video source: https://ke.biowolf.cn
-######ÉúĞÅ×ÔÑ§Íø: https://www.biowolf.cn/
-######Î¢ĞÅ¹«ÖÚºÅ£ºbiowolf_cn
-######ºÏ×÷ÓÊÏä£ºbiowolf@foxmail.com
-######´ğÒÉÎ¢ĞÅ: 18520221056
 
 #if (!require("BiocManager"))
 #    install.packages("BiocManager")
 #BiocManager::install("maftools")
 
 
-library(maftools)       #ÒıÓÃ°ü
-setwd("D:\\biowolf\\m6aTME\\45.maftools")      #ÉèÖÃ¹¤×÷Ä¿Â¼
+library(maftools)       #å¼•ç”¨åŒ…
+setwd("D:\\biowolf\\m6aTME\\45.maftools")      #è®¾ç½®å·¥ä½œç›®å½•
 
-#¶ÁÈ¡m6AÆÀ·ÖµÄ·Ö×éÎÄ¼ş
+#è¯»å–m6Aè¯„åˆ†çš„åˆ†ç»„æ–‡ä»¶
 score=read.table("m6Ascore.group.txt", header=T, sep="\t", check.names=F)
 outTab=score[,c(1, ncol(score))]
 colnames(outTab)=c("Tumor_Sample_Barcode", "m6Ascore")
 write.table(outTab, file="ann.txt", sep="\t", quote=F, row.names=F)
 
-#¶ÁÈ¡»ùÒòÍ»±äÎÄ¼ş
+#è¯»å–åŸºå› çªå˜æ–‡ä»¶
 geneNum=20
 geneMut=read.table("geneMut.txt", header=T, sep="\t", check.names=F, row.names=1)
 gene=row.names(geneMut)[1:geneNum]
 
-#ÑÕÉ«
+#é¢œè‰²
 ann_colors=list()
 col=c("#0066FF","#FF0000")
 names(col)=c("Low", "High")
 ann_colors[["m6Ascore"]]=col
 
-#µÍÆÀ·Ö×éÆÙ²¼Í¼
+#ä½è¯„åˆ†ç»„ç€‘å¸ƒå›¾
 pdf(file="low.pdf", width=6, height=6)
 maf=read.maf(maf="low.maf", clinicalData="ann.txt")
 oncoplot(maf=maf, clinicalFeatures="m6Ascore", genes=gene, annotationColor=ann_colors, keepGeneOrder=T)
 dev.off()
 
-#¸ßÆÀ·Ö×éÆÙ²¼Í¼
+#é«˜è¯„åˆ†ç»„ç€‘å¸ƒå›¾
 pdf(file="high.pdf", width=6, height=6)
 maf=read.maf(maf="high.maf", clinicalData="ann.txt")
 oncoplot(maf=maf, clinicalFeatures="m6Ascore", genes=gene, annotationColor=ann_colors, keepGeneOrder=T)
 dev.off()
-
-
-######Video source: https://ke.biowolf.cn
-######ÉúĞÅ×ÔÑ§Íø: https://www.biowolf.cn/
-######Î¢ĞÅ¹«ÖÚºÅ£ºbiowolf_cn
-######ºÏ×÷ÓÊÏä£ºbiowolf@foxmail.com
-######´ğÒÉÎ¢ĞÅ: 18520221056
