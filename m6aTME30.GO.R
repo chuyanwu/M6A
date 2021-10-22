46a18b3f2bfa4ffe471fead9680db19e387c26cf
chuyanwu <92982961+chuyanwu@users.noreply.github.com>
2021-10-22 22:25:31 +0800

    R script

---
+++ m6aTME30.GO.R
@@ -0,0 +1,74 @@
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
+
+#install.packages("colorspace")
+#install.packages("stringi")
+#install.packages("ggplot2")
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#    install.packages("BiocManager")
+#BiocManager::install("org.Hs.eg.db")
+#BiocManager::install("DOSE")
+#BiocManager::install("clusterProfiler")
+#BiocManager::install("enrichplot")
+
+
+#引用包
+library("clusterProfiler")
+library("org.Hs.eg.db")
+library("enrichplot")
+library("ggplot2")
+
+pvalueFilter=0.05       #p值过滤条件
+qvalueFilter=0.05       #矫正后的p值过滤条件
+
+#定义颜色
+colorSel="qvalue"
+if(qvalueFilter>0.05){
+	colorSel="pvalue"
+}
+
+setwd("D:\\biowolf\\m6aTME\\30.GO")        #设置工作目录
+rt=read.table("interGene.txt", header=F, sep="\t", check.names=F)     #读取输入文件
+
+#基因名字转换为基因id
+genes=unique(as.vector(rt[,1]))
+entrezIDs=mget(genes, org.Hs.egSYMBOL2EG, ifnotfound=NA)
+entrezIDs=as.character(entrezIDs)
+gene=entrezIDs[entrezIDs!="NA"]        #去除基因id为NA的基因
+#gene=gsub("c\\(\"(\\d+)\".*", "\\1", gene)
+
+#GO富集分析
+kk=enrichGO(gene=gene, OrgDb=org.Hs.eg.db, pvalueCutoff=1, qvalueCutoff=1, ont="all", readable=T)
+GO=as.data.frame(kk)
+GO=GO[(GO$pvalue<pvalueFilter & GO$qvalue<qvalueFilter),]
+#保存富集结果
+write.table(GO, file="GO.txt", sep="\t", quote=F, row.names = F)
+
+#定义显示Term数目
+showNum=10
+if(nrow(GO)<30){
+	showNum=nrow(GO)
+}
+
+#柱状图
+pdf(file="barplot.pdf", width=9, height=7)
+bar=barplot(kk, drop=TRUE, showCategory=showNum, split="ONTOLOGY", color=colorSel) + facet_grid(ONTOLOGY~., scale='free')
+print(bar)
+dev.off()
+		
+#气泡图
+pdf(file="bubble.pdf", width=9, height=7)
+bub=dotplot(kk, showCategory=showNum, orderBy="GeneRatio", split="ONTOLOGY", color=colorSel) + facet_grid(ONTOLOGY~., scale='free')
+print(bub)
+dev.off()
+
+
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
