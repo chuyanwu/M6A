2ef7800d0813279cdb358613a1bb9ecc51d73bd0
chuyanwu <92982961+chuyanwu@users.noreply.github.com>
2021-10-22 22:27:58 +0800

    R script

---
+++ m6aTME34.geneClusterSur.R
@@ -0,0 +1,69 @@
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
+
+#install.packages("survival")
+#install.packages("survminer")
+
+
+#引用包
+library(survival)
+library(survminer)
+clusterFile="geneCluster.txt"     #基因分型文件
+cliFile="time.txt"                #生存数据文件
+setwd("D:\\biowolf\\m6aTME\\34.geneClusterSur")      #设置工作目录
+
+#读取输入文件
+cluster=read.table(clusterFile, header=T, sep="\t", check.names=F, row.names=1)
+rownames(cluster)=gsub("(.*?)\\_(.*?)", "\\2", rownames(cluster))
+cli=read.table(cliFile, header=T, sep="\t", check.names=F, row.names=1)
+colnames(cli)=c("futime", "fustat")
+cli$futime=cli$futime/365
+
+#数据合并
+sameSample=intersect(row.names(cluster), row.names(cli))
+rt=cbind(cli[sameSample,,drop=F], cluster[sameSample,,drop=F])
+
+#生存差异统计
+length=length(levels(factor(rt$geneCluster)))
+diff=survdiff(Surv(futime, fustat) ~ geneCluster, data = rt)
+pValue=1-pchisq(diff$chisq, df=length-1)
+if(pValue<0.001){
+	pValue="p<0.001"
+}else{
+	pValue=paste0("p=",sprintf("%.03f",pValue))
+}
+fit <- survfit(Surv(futime, fustat) ~ geneCluster, data = rt)
+#print(surv_median(fit))
+
+#绘制生存曲线
+bioCol=c("#0066FF","#FF9900","#FF0000","#6E568C","#7CC767","#223D6C","#D20A13","#FFD121","#088247","#11AA4D")
+bioCol=bioCol[1:length(levels(factor(rt[,"geneCluster"])))]
+surPlot=ggsurvplot(fit, 
+		           data=rt,
+		           conf.int=F,
+		           pval=pValue,
+		           pval.size=6,
+		           legend.labs=levels(factor(rt[,"geneCluster"])),
+		           legend.title="geneCluster",
+		           legend = c(0.8, 0.8),
+		           font.legend=10,
+		           xlab="Time(years)",
+		           break.time.by = 1,
+		           palette = bioCol,
+		           surv.median.line = "hv",
+		           risk.table=T,
+		           cumevents=F,
+		           risk.table.height=.25)
+pdf(file="survival.pdf", onefile = FALSE, width=7, height=5.5)
+print(surPlot)
+dev.off()
+
+
+######Video source: https://ke.biowolf.cn
+######生信自学网: https://www.biowolf.cn/
+######微信公众号：biowolf_cn
+######合作邮箱：biowolf@foxmail.com
+######答疑微信: 18520221056
